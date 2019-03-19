966b2a1b35b143badfea2799ac0cdbce7aadf91c
Adam Dierkens <Adam_Dierkens@intuit.com>
2019-03-18 18:10:24 -0700

    Fix undef value

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,26 @@
 import addons from '@storybook/addons';
 import { FORCE_RE_RENDER, STORY_CHANGED } from '@storybook/core-events';
 
-function forceReRender() {
-  addons.getChannel().emit(FORCE_RE_RENDER);
-}
-
-addons.getChannel().addListener(STORY_CHANGED, () => {
-  clear();
-});
-
 let stores: {
   [name: string]: any;
 } = {};
 
-export function clear() {
+addons.getChannel().addListener(STORY_CHANGED, () => {
   stores = {};
-}
+});
 
 export default function useState<T>(
   name: string,
   store: T
 ): [T, (newState: T) => void] {
-  const actualVals = stores[name] || store;
+  const actualVals = stores[name] === undefined ? store : stores[name];
   stores[name] = actualVals;
 
   return [
     actualVals,
     (newState: T) => {
       stores[name] = newState;
-      forceReRender();
+      addons.getChannel().emit(FORCE_RE_RENDER);
     }
   ];
 }
